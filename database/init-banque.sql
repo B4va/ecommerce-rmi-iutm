ea057be77fba954fcfcb299768f850b329251247
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2021-02-14 17:41:47 +0100

    Fix init-banque.sql

--- database/init-banque.sql
+++ database/init-banque.sql
@@ -2,10 +2,9 @@ CREATE TABLE compte_bancaire(
 	id SERIAL PRIMARY KEY,
 	reference VARCHAR,
 	solde FLOAT
-)
+);
 
-INSERT INTO compte_bancaire values (reference,solde) ('9999 9999 9999 9999', 1000.50)
-INSERT INTO compte_bancaire values (reference,solde) ('6666 6666 6666 6666', 666.66)
-INSERT INTO compte_bancaire values (reference,solde) ('1010 1001 1011 0101', 10.30)
-INSERT INTO compte_bancaire values (reference,solde) ('9876 5432 1987 6543', 0.00)
-
+INSERT INTO compte_bancaire (reference, solde) values ('9999 9999 9999 9999', 1000.50);
+INSERT INTO compte_bancaire (reference, solde) values ('6666 6666 6666 6666', 666.66);
+INSERT INTO compte_bancaire (reference, solde) values ('1010 1001 1011 0101', 10.30);
+INSERT INTO compte_bancaire (reference, solde) values ('9876 5432 1987 6543', 0.00);
