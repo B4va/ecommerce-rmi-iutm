20c8762b760b32d586c956318a8f6597c5e2a3fb
alexisbe-github <66599552+alexisbe-github@users.noreply.github.com>
2021-02-14 17:24:49 +0100

    Script d'initialisation de la bdd Banque

--- database/init-banque.sql
+++ database/init-banque.sql
@@ -1,3 +1,11 @@
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY
-);
+CREATE TABLE compte_bancaire(
+	id SERIAL PRIMARY KEY,
+	reference VARCHAR,
+	solde FLOAT
+)
+
+INSERT INTO compte_bancaire values (reference,solde) ('9999 9999 9999 9999', 1000.50)
+INSERT INTO compte_bancaire values (reference,solde) ('6666 6666 6666 6666', 666.66)
+INSERT INTO compte_bancaire values (reference,solde) ('1010 1001 1011 0101', 10.30)
+INSERT INTO compte_bancaire values (reference,solde) ('9876 5432 1987 6543', 0.00)
+
