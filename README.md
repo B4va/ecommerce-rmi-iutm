fb6fa1224999e44c6d3a750c1d2a9799166f67e8
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2021-02-23 20:28:42 +0100

    Maj readme

--- README.md
+++ README.md
@@ -31,3 +31,23 @@ $ docker container rm client magasin banque db-magasin db-banque
 ```
 $ docker image rm ecommerce-rmi-iutm_client ecommerce-rmi-iutm_magasin ecommerce-rmi-iutm_banque
 ```
+
+## Conception
+
+### Cas d'utilisation
+
+![cas d'utilisation](img-readme/conception-projet-cas-dutilisation.png)
+
+### Architecture générale
+
+![architecture](img-readme/conception-projet-archi.png)
+
+### Classes
+
+![classes-1](img-readme/conception-projet-apps-1.png)
+![classes-1](img-readme/conception-projet-apps-2.png)
+![classes-3](img-readme/conception-projet-apps-3.png)
+
+### Données
+
+![données](img-readme/conception-projet-donnees.png)
--- client/src/main/java/controleurs/ListeMagasinsControleur.java
+++ client/src/main/java/controleurs/ListeMagasinsControleur.java
@@ -2,6 +2,7 @@ package controleurs;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,7 +20,9 @@ public class ListeMagasinsControleur extends Controleur {
   public Scene getScene() {
     Text label = new Text("Magasins");
     VBox layout = new VBox(20);
-    layout.getChildren().addAll(label);
+    Button button = new Button("Retour page de connexion");
+    button.setOnAction(e -> primaryStage.setScene(new ConnexionControleur(primaryStage).getScene()));
+    layout.getChildren().addAll(label, button);
     layout.setAlignment(Pos.CENTER);
     return new Scene(layout, 300, 250);
   }
