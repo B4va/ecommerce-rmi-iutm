6f5b30a5b9a486dc94466ac6c4bf91b3ef7d1ab7
Pythoud-dev <61004682+Pythoud-dev@users.noreply.github.com>
2021-03-01 11:14:00 +0100

    Insertions dans la bdd Magasin

--- database/init-magasin.sql
+++ database/init-magasin.sql
@@ -54,3 +54,25 @@ CREATE TABLE article_panier (
     CONSTRAINT fk_panier FOREIGN KEY (panier_id) REFERENCES panier(id),
     CONSTRAINT fk_article_panier FOREIGN KEY (article_id) REFERENCES article(id)
 );
+
+INSERT INTO client (nom, prenom, mail, mot_de_passe) values ('Smith', 'John', 'john@mail.com', '1234');
+
+INSERT INTO boutique (nom) VALUES ('Match');
+INSERT INTO boutique (nom) VALUES ('Bricomarché');
+INSERT INTO boutique (nom) VALUES ('Auchan');
+
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('pomme', 1.20, 50, 'Pomme rouge bien mure 1.20€/unité', null, 1);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('banane', 1.00, 25, 'Banane bien mure 1.00€/unité', null, 1);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('orange', 1.50, 30, 'Orange bien mure 1.50€/unité', null, 1);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('fraise', 0.10, 100, 'Fraise bien juteuse 0.10€/unité', null, 1);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('salade', 0.50, 15, 'Salade verte fraiche 0.50€/unité', null, 1);
+
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('marteau', 5.20, 10, 'Marteau en acier 5.20€/unité', null, 2);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('clou', 0.05, 100, 'Clou en acier 0.05€/unité', null, 2);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('perceuse', 75.99, 5, 'Peceuse hight-tech 75.99€/unité', null, 2);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('Tournevis', 12.30, 15, 'Tournevis en acier 12.30€/unité', null, 2);
+
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('Ordinateur portable HP', 399.20, 7, 'Ordinateur HP Intel Core I3  399.20€/unité', null, 3);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('Télévision 32 pouce', 299.99, 3, 'Ecran LG 32 pouce 299.99€/unité', null, 3);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('Tablette Samsung', 199.00, 5, 'Tablette Samsung 32 Go, 3Go RAM  199.00€/unité', null, 3);
+INSERT INTO article (libelle, prix, stock, description, image_blob, boutique_id) VALUES ('Portable', 5.20, 10, 'Marteau en acier 5.20€/unité', null, 2);
